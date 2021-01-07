2a85690e73f6d93cf7049ea65e6d82f4f9dd3d63
Oromion <caznaranl@uni.pe>
2021-01-07 07:48:02 -0500

    Primeira confirmação

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Algebra-linear-II
+# [Álgebra Linear II](https://moodle.mat.unb.br/20201)
+
+Um dos [cursos](https://www.mat.unb.br/verao2021/verao/verao_pt.html)
+de pós-graduação serão oferecidos na XLIX Escola de Verão do
+Departamento de Matemática da Universidade de Brasília.
+
+## Dias e horários
+
+Segunda à Sexta de 10h às 12h.
+
+## Período
+
+04 de janeiro a 12 de fevereiro de 2021.
+
+## Ementa
+
+1. Sistemas de equações lineares.
+2. Espaços vetoriais.
+3. Polinômios.
+4. Decomposição primária.
+5. Forma canônica de Jordan.
+6. Teorema espectral.
+7. Produto interno.
+8. Formas multilineares – tensores.
+
+## Bibliografia
+
+1. Hoffman/Kunze; Álgebra Linear; Ed. Livros Técnicos e Científicos, 1979;
+2. Lang, Serge; Álgebra Linear; Ed. Ciência Moderna, 2003;
+3. Halmos, P. Espaços Vetoriais de Dimensão Finita; Ed. Campus, 1978;
+4. Lipschutz, S; Álgebra Linear; Ed. McGraw-Hill Makron Books 1994.
---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,80 @@
+const { description } = require("../../package");
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: "Álgebra Linear II",
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  theme: "default-prefers-color-scheme",
+  themeConfig: {
+    overrideTheme: "dark",
+    repo: "carlosal1015/Algebra-linear-II",
+    editLinks: false,
+    docsDir: "",
+    editLinkText: "",
+    lastUpdated: false,
+    nav: [
+      {
+        text: "Guide",
+        link: "/guide/",
+      },
+      {
+        text: "Config",
+        link: "/config/",
+      },
+      {
+        text: "Moodle",
+        link: "https://moodle.mat.unb.br/20201",
+      },
+      {
+        text: "XLIX Escola de Verão MAT/UnB",
+        link: "https://www.mat.unb.br/verao2021/verao/verao_pt.html",
+      },
+    ],
+    sidebar: {
+      "/guide/": [
+        {
+          title: "Guide",
+          collapsable: false,
+          children: ["", "using-vue"],
+        },
+      ],
+    },
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    "@vuepress/plugin-back-to-top",
+    "@vuepress/plugin-medium-zoom",
+    "@maginapp/vuepress-plugin-katex",
+    {
+      delimiters: "dollars",
+    },
+  ],
+};
